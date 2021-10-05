948bb236c975447c6ec19be7f16600197795c5a7
Alex Israels <alex.israels@amway.com>
2021-10-05 00:36:21 -0400

    ngSwitch

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { FavoriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  courses = [1];
+  viewMode = 'somethingElse';
 }
