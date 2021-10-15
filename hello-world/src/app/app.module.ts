2a11c954fda8c6d887b88fc5d3f43940827bb97c
aiuhjv1 <alex.israels@amway.com>
2021-10-15 11:12:48 -0400

    template driven forms assignment

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     InputFormatDirective,
     ZippyComponent,
     ContactFormComponent,
+    CourseFormComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CoursesService],
