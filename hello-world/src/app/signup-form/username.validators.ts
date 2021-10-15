e4c9b1e40d98db5fc8c5170e751e26057c168289
aiuhjv1 <alex.israels@amway.com>
2021-10-15 15:10:03 -0400

    async validators

--- hello-world/src/app/signup-form/username.validators.ts
+++ hello-world/src/app/signup-form/username.validators.ts
@@ -7,4 +7,18 @@ export class UsernameValidators {
     }
     return null;
   }
+
+  static async shouldBeUnique(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'mosh') {
+          resolve({ shouldBeUnique: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+  }
 }
