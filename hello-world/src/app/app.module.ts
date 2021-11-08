ebfbe4bc2542f715b97d20989e46f031de17097e
aiuhjv1 <91083393+aiuhjv1@users.noreply.github.com>
2021-11-08 13:23:44 -0500

    github followers exrecise

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { ChangePasswordFormComponent } from './change-password-form/change-passw
 import { PostsComponent } from './posts/posts.component';
 import { PostService } from './services/post.service';
 import { AppErrorHandler } from './common/app-error-handler';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { GithubFollowersService } from './github-followers.service';
 
 @NgModule({
   declarations: [
@@ -40,6 +42,7 @@ import { AppErrorHandler } from './common/app-error-handler';
     NewCourseFormComponent,
     ChangePasswordFormComponent,
     PostsComponent,
+    GithubFollowersComponent,
   ],
   imports: [
     BrowserModule,
@@ -51,6 +54,7 @@ import { AppErrorHandler } from './common/app-error-handler';
   providers: [
     CoursesService,
     PostService,
+    GithubFollowersService,
     { provide: ErrorHandler, useClass: AppErrorHandler },
   ],
   bootstrap: [AppComponent],
