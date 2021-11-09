433b480220c2f2ff921bb50dc28e8611d8d429fe
aiuhjv1 <91083393+aiuhjv1@users.noreply.github.com>
2021-11-09 15:27:06 -0500

    subscribing to multiple observables

--- hello-world/src/app/github-followers/github-followers.component.ts
+++ hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../github-followers.service';
+import { Observable, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'github-followers',
   templateUrl: './github-followers.component.html',
-  styleUrls: ['./github-followers.component.css']
+  styleUrls: ['./github-followers.component.css'],
 })
 export class GithubFollowersComponent implements OnInit {
   followers: any[] = [];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService
+  ) {}
 
   ngOnInit(): void {
-    this.service.getAll().subscribe(followers => this.followers = followers)
-  }
+    forkJoin([this.route.paramMap, this.route.queryParamMap]).subscribe(
+      (combined) => {
+        let id = combined[0].get('id');
+        let page = combined[1].get('page');
 
+        // example of how parameters would be used
+        // this.service.getAll({ id: id, page: page });
+      }
+    );
+    this.service
+      .getAll()
+      .subscribe((followers) => (this.followers = followers));
+  }
 }
