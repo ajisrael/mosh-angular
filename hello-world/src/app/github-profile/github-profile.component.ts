f7766cff39f578711b544d9c8d597af7e89964b0
aiuhjv1 <91083393+aiuhjv1@users.noreply.github.com>
2021-11-11 11:15:11 -0500

    programmatic navigation

--- hello-world/src/app/github-profile/github-profile.component.ts
+++ hello-world/src/app/github-profile/github-profile.component.ts
@@ -1,24 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css'],
 })
-export class GithubProfileComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+export class GithubProfileComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    // use when navigating to another page between instances
-    // let id = this.route.snapshot.paramMap.get('id');
-    // console.log(id);
-
-    // use when component should stay loaded between pages
-    this.route.paramMap.subscribe((params) => {
-      let id = params.get('id');
-      let idNum: number = id ? Number.parseInt(id) : 0;
-      console.log(idNum);
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' },
     });
   }
 }
